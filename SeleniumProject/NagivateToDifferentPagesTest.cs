502e2f901412f175fdbc9a3163271d7c15a04574
Peter Floyd <10535993@mydbs.ie>
2021-04-12 23:53:42 +0100

    updated post deployement tests

--- SeleniumProject/NagivateToDifferentPagesTest.cs
+++ SeleniumProject/NagivateToDifferentPagesTest.cs
@@ -11,26 +11,34 @@ using OpenQA.Selenium.Remote;
 using OpenQA.Selenium.Support.UI;
 using OpenQA.Selenium.Interactions;
 using NUnit.Framework;
-[TestFixture]
-public class NagivateToDifferentPagesTest {
-  private IWebDriver driver;
-  public IDictionary<string, object> vars {get; private set;}
-  private IJavaScriptExecutor js;
-  [SetUp]
-  public void SetUp() {
-    driver = new RemoteWebDriver(new Uri("http://localhost:4444/wd/hub"), new ChromeOptions().ToCapabilities());
-    js = (IJavaScriptExecutor)driver;
-    vars = new Dictionary<string, object>();
-  }
-  [TearDown]
-  protected void TearDown() {
-    driver.Quit();
-  }
-  [Test]
-  public void nagivateToDifferentPages() {
-    driver.Navigate().GoToUrl("http://localhost:4200/home");
-    driver.Manage().Window.Size = new System.Drawing.Size(1936, 1056);
-    driver.FindElement(By.CssSelector(".nav:nth-child(1)")).Click();
-    driver.FindElement(By.CssSelector(".nav:nth-child(2)")).Click();
-  }
+
+namespace SeleniumProject
+{
+    [TestFixture]
+    public class NagivateToDifferentPagesTest
+    {
+        private IWebDriver driver;
+        public IDictionary<string, object> vars { get; private set; }
+        private IJavaScriptExecutor js;
+        [SetUp]
+        public void SetUp()
+        {
+            driver = new RemoteWebDriver(new Uri("http://localhost:4444/wd/hub"), new ChromeOptions().ToCapabilities());
+            js = (IJavaScriptExecutor)driver;
+            vars = new Dictionary<string, object>();
+        }
+        [TearDown]
+        protected void TearDown()
+        {
+            driver.Quit();
+        }
+        [Test]
+        public void nagivateToDifferentPages()
+        {
+            driver.Navigate().GoToUrl("http://20.67.220.11/home");
+            driver.Manage().Window.Size = new System.Drawing.Size(1936, 1056);
+            driver.FindElement(By.CssSelector(".nav:nth-child(1)")).Click();
+            driver.FindElement(By.CssSelector(".nav:nth-child(2)")).Click();
+        }
+    }
 }
--- SeleniumProject/Program.cs
+++ SeleniumProject/Program.cs
@@ -14,17 +14,18 @@ namespace SeleniumProject
         {           
             
             IWebDriver driver = new ChromeDriver("C:\\selenium\\Chrome");
-            
 
-            driver.Navigate().GoToUrl("https://www.google.com");
-            driver.FindElement(By.Name("q")).SendKeys("Dublin Business School");
-            driver.SwitchTo().Frame(0);
-            driver.FindElement(By.CssSelector("#introAgreeButton > span > span")).Click();
-
-            driver.FindElement(By.Name("btnK")).Click();
-            driver.FindElement(By.CssSelector("div:nth-child(2) > .tF2Cxc:nth-child(2) .LC20lb")).Click();
-            driver.FindElement(By.Id("usrCtrl_Hide")).Click();
-            driver.FindElement(By.LinkText("Courses")).Click();
+            driver.Navigate().GoToUrl("http://localhost:4200/login");
+            driver.Manage().Window.Size = new System.Drawing.Size(1936, 1056);
+            //driver.FindElement(By.CssSelector("#introAgreeButton > span > span")).Click();
+            driver.FindElement(By.CssSelector(".registerLink")).Click();
+            driver.FindElement(By.CssSelector(".firstname")).Click();
+            driver.FindElement(By.CssSelector(".firstname")).SendKeys("Jack");
+            driver.FindElement(By.CssSelector(".lastname")).SendKeys("Reilly");
+            driver.FindElement(By.CssSelector(".username")).SendKeys("jockreilly@gmail.com");
+            driver.FindElement(By.CssSelector(".password")).SendKeys("testpass");
+            driver.FindElement(By.CssSelector(".avatar")).SendKeys("testavatar");
+            driver.FindElement(By.CssSelector(".login")).Click();
         }
     }
 }
